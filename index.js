5639759631947e3d847c7a1bda54242de7ec0abc
yazen firas <psyazen3@gmail.com>
2020-05-19 13:01:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  
+  let f=getElementById("")
 });
 console.log(
   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
