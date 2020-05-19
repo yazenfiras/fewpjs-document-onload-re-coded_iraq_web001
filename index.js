a7073ee5789f86bdb023b1891cc1baf0e54ed5d3
yazen firas <psyazen3@gmail.com>
2020-05-19 12:59:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
 });
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
