77f309f556fbfd3a4738144a67bb343f59927dba
yazen firas <psyazen3@gmail.com>
2020-05-19 13:13:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  
+  let f=document.getElementById("text");
+  
+  console.log(f);
+  f.innerHTML="This is really cool!";
+});
